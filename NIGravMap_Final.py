68e0d02a5aded9f7ce57463242f7c2ed993b7b29
M8434 <163770856+M8434@users.noreply.github.com>
2024-05-14 23:35:11 +0100

    More Edits

--- NIGravMap_Final.py
+++ NIGravMap_Final.py
@@ -12,12 +12,15 @@ import folium
 # ---------------------------------------------------------------------------------------------------------------------
 
 df = pd.read_csv('Ulst6Jan87.csv') #historic gravity data
-df.head()
-df['geometry'] = list(zip(df['LONG'], df['LAT'])) 
-df['geometry'] = df['geometry'].apply(Point) 
-gdf = gpd.GeoDataFrame(df)
-gdf = gdf.set_crs("EPSG:4326") 
+HistGravData = gpd.GeoDataFrame(df[['id', 'OBS (mgal)']], #Keep only the ID and the reading in milligals.
+                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') 
+
 #gdf.to_file('HistoricGravityPoints.shp') #Creates .shp for use in ArcPro etc
+
+df = pd.read_csv('OSNIAbsoluteGravity.csv') #Absolute Gravity Data
+HistGravData = gpd.GeoDataFrame(df[['Station Name'], #Keep only Station Name
+                            geometry=gpd.points_from_xy(df['Easting'], df['Northing']),crs='epsg:2157') #Source data is Irish Transverse Mercator
+
 
 df = pd.read_csv('OSNIAbsoluteGravity.csv')
 del df['Easting.1'], df['Northing.1'], df['Unnamed: 8'], df['latitude'], df ['longitude'] #Removal of superflous coloumns
