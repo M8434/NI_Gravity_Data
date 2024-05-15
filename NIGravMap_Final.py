e7cd35f714e79d50c0048ba371b28d197e75ef0b
M8434 <163770856+M8434@users.noreply.github.com>
2024-05-15 17:08:09 +0100

    Tidying Script
    Tidying script and attempting to make the Folium map pretty

--- NIGravMap_Final.py
+++ NIGravMap_Final.py
@@ -6,23 +6,52 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 import folium
+import io
+from PIL import Image
+
 
 # ---------------------------------------------------------------------------------------------------------------------
-# Preparation of source data
+# Preparation of input data
 # ---------------------------------------------------------------------------------------------------------------------
 
 df = pd.read_csv('Ulst6Jan87.csv') #historic gravity data
 HistGravData = gpd.GeoDataFrame(df[['id', 'OBS (mgal)']], #Keep only the ID and the reading in milligals.
                             geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') 
+HistGravData.to_crs("EPSG:4326")
+HistGravData.to_file('Hist.shp',crs='EPSG:4326') #specified crs as by default it was creating .shp with source CRS
 
-#HistGravData.to_file('HistoricGravityPoints.shp') #Creates .shp for use in ArcPro etc
 
 df = pd.read_csv('OSNIAbsoluteGravity.csv') #Absolute Gravity Data
-HistGravData = gpd.GeoDataFrame(df[['Station Name'], #Keep only Station Name
-                            geometry=gpd.points_from_xy(df['Easting'], df['Northing']),crs='epsg:2157') #Source data is Irish Transverse Mercator
+AbsGravData = gpd.GeoDataFrame(df['Station Name'], #Keep only Station Name
+                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') 
+AbsGravData.to_file('AbData.shp',crs='EPSG:4326') #Will throw UserWarning about column name length, this is OK - **future dev work**
 
-AbsGravData.to_crs ("EPSG:4326") #Transformation to WGS84 for interoperability (https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.to_crs.html)
+# ---------------------------------------------------------------------------------------------------------------------
+# Spatial Analysis to identify historic gravity points within 5km of absolute gravity point
+# ---------------------------------------------------------------------------------------------------------------------
 
-#AbsGravData.to_file('AbsoluteGravityPoints.shp') #Creates .shp for use in ArcPro etc
+Hist1 = gpd.read_file('Hist.shp')
+AbData = gpd.read_file('AbData.shp')
+ABbuffer = AbData.buffer(0.1) #Figure represents 10km
+ABbuffer.to_file('AB_buffer.shp',crs='EPSG:4326') 
+AB_buffer = gpd.read_file('AB_buffer.shp')
+AB_buffer.to_crs('EPSG:4326')
+CombinedBuffer = AB_buffer.geometry.unary_union
+within_CombinedBuffer = Hist1[Hist1.geometry.within(CombinedBuffer)]
+within_CombinedBuffer.head()
+within_CombinedBuffer.to_file('withincombined.shp',crs='EPSG:4326')
 
+# ---------------------------------------------------------------------------------------------------------------------
+# Configuration of Folium Map
+# ---------------------------------------------------------------------------------------------------------------------
+
+m = folium.Map(tiles=None)
+
+AbsGravData.explore(m=m, 'Station Name',marker_type='marker', popup=True, legend=False,)
+
+AB_buffer.explore(m=m, popup=False, fill_color = "White", legend_name = "10k buffer of Absolute Gravity Stations",)
+
+within_CombinedBuffer.explore(m=m, popup=True, legend=False,)
+
+m
 
