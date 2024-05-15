6cf27415049ca8f3594f92499380d1ce1416a126
M8434 <163770856+M8434@users.noreply.github.com>
2024-05-15 20:28:07 +0100

    DEADLINE COMMIT
    Final Commit before project deadline

--- NIGravMap_Final.py
+++ NIGravMap_Final.py
@@ -6,50 +6,44 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 import folium
-import io
-from PIL import Image
-
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Preparation of input data
 # ---------------------------------------------------------------------------------------------------------------------
 
 df = pd.read_csv('Ulst6Jan87.csv') #historic gravity data
-HistGravData = gpd.GeoDataFrame(df[['id', 'OBS (mgal)']], #Keep only the ID and the reading in milligals.
-                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') 
-HistGravData.to_crs("EPSG:4326")
-HistGravData.to_file('Hist.shp',crs='EPSG:4326') #specified crs as by default it was creating .shp with source CRS
+Hist = gpd.GeoDataFrame(df[['id', 'OBS (mgal)']], #Keep only the ID and the reading in milligals.
+                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') #OpenStreetMap
 
+Hist.to_crs("EPSG:4326")
+Hist.to_file('HistGravData.shp',crs='EPSG:4326') #specified crs as by default it was creating .shp with source CRS
 
 df = pd.read_csv('OSNIAbsoluteGravity.csv') #Absolute Gravity Data
-AbsGravData = gpd.GeoDataFrame(df['Station Name'], #Keep only Station Name
-                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326') 
-AbsGravData.to_file('AbData.shp',crs='EPSG:4326') #Will throw UserWarning about column name length, this is OK - **future dev work**
-
+Absolute = gpd.GeoDataFrame(df[['Station Name','GNSS Observation Date']], #Keep only Station Name
+                            geometry=gpd.points_from_xy(df['LONG'], df['LAT']),crs='epsg:4326')
+Absolute.rename(columns={'Station Name': 'Station', 'GNSS Observation Date': 'ObDate'}, inplace=True) #Current column names too long for .shp
+Absolute.to_crs('EPSG:29902')
+Absolute.to_file('AbsGravData.shp',crs='EPSG:29902')
 # ---------------------------------------------------------------------------------------------------------------------
-# Spatial Analysis to identify historic gravity points within 5km of absolute gravity point
+# Spatial Analysis to identify historic gravity points within 5km of Absolute Gravity Point
 # ---------------------------------------------------------------------------------------------------------------------
 
-Hist1 = gpd.read_file('Hist.shp')
-AbData = gpd.read_file('AbData.shp')
-ABbuffer = AbData.buffer(0.1) #Figure represents 10km
+AbsGravData = gpd.read_file('AbsGravData.shp')
+ABbuffer = AbsGravData.buffer(0.1) #Figure represents 10km #Figure represents 10km. Need to buffer on .shp as CRS is projected
+ABbuffer.to_crs('EPSG:4326')
 ABbuffer.to_file('AB_buffer.shp',crs='EPSG:4326') 
-AB_buffer = gpd.read_file('AB_buffer.shp')
-AB_buffer.to_crs('EPSG:4326')
-CombinedBuffer = AB_buffer.geometry.unary_union
+ABbuffer = gpd.read_file('AB_buffer.shp')
+
+CombinedBuffer = ABbuffer.geometry.unary_union #Created to simplify process
 within_CombinedBuffer = Hist1[Hist1.geometry.within(CombinedBuffer)]
-within_CombinedBuffer.head()
-within_CombinedBuffer.to_file('withincombined.shp',crs='EPSG:4326')
-
+within_CombinedBuffer.to_file('HistGrav10km_within_AbsoluteGrav.shp',crs='EPSG:4326')
 # ---------------------------------------------------------------------------------------------------------------------
-# Configuration of Folium Map
+# Configuration of basic Folium Map
 # ---------------------------------------------------------------------------------------------------------------------
 
-m = folium.Map(tiles=None)
+m = AbsGravData.explore('Station Name',marker_type='marker', popup=True, legend=False, tiles= "cartodb positron")
 
-AbsGravData.explore(m=m, 'Station Name',marker_type='marker', popup=True, legend=False,)
-
-AB_buffer.explore(m=m, popup=False, fill_color = "White", legend_name = "10k buffer of Absolute Gravity Stations",)
+AB_buffer.explore(m=m, popup=False, fill_color = "White",)
 
 within_CombinedBuffer.explore(m=m, popup=True, legend=False,)
 
